e748911aa69d5ce79cfea18227af05d0d81642dd
Vinoth-2000 <vinothsathish2000@gmail.com>
2022-06-24 09:46:47 +0530

    project

---
+++ CrudOperations/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudOperations
+{
+    public class ProductContext:DbContext
+
+    {
+        public ProductContext():base("dbconn")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
